dd8ae8fb63ce13bf5d371d46a48953f02b7d68b7
b.k.hemasitha <hemasitha20@gmail.com>
2026-06-09 20:24:13 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+
+        System.out.println(sum);
+    }
+}
